3c05df0eaaf7d31e60115d67fc6060435d40c217
randomicons <srirambhat7@gmail.com>
2024-10-31 16:50:20 -0500

    interactions for api/interactions only

--- src/main/java/com/bigschlong/demo/controllers/InteractionsController.java
+++ src/main/java/com/bigschlong/demo/controllers/InteractionsController.java
@@ -1,14 +1,12 @@
 package com.bigschlong.demo.controllers;
 
-import com.bigschlong.demo.models.NbaPlayer;
 import com.bigschlong.demo.models.PingModel;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.UUID;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/interactions")
+@RequestMapping("/api/interactions ")
 public class InteractionsController {
 
     @PostMapping(value = "/", produces = "application/json")
