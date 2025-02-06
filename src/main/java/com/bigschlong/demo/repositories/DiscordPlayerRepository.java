95fff273eec7594b8c256413d53942bac64fa144
edk12564 <edward.kang12@gmail.com>
2025-02-06 13:30:02 -0600

    added back some repos

--- src/main/java/com/bigschlong/demo/repositories/DiscordPlayerRepository.java
+++ src/main/java/com/bigschlong/demo/repositories/DiscordPlayerRepository.java
@@ -1,23 +1,23 @@
-//package com.bigschlong.demo.repositories;
-//
-//import com.bigschlong.demo.models.dtos.DiscordPlayer;
-//import com.bigschlong.demo.models.dtos.NbaPlayer;
-//import org.springframework.data.jdbc.repository.query.Query;
-//import org.springframework.data.repository.CrudRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.List;
-//import java.util.UUID;
-//
-//@Repository
-//public interface DiscordPlayerRepository extends CrudRepository<DiscordPlayer, UUID> {
-//
-//    @Query(value = """
-//    SELECT np.* FROM daily_roster dr
-//    JOIN discord_players dp on dp.discord_player_uid = dr.discord_player_uid
-//    JOIN  nba_players np on np.nba_player_uid = dr.nba_player_uid
-//    WHERE dp.discord_player_uid = :discordId
-//    """)
-//    List<NbaPlayer> getRosterByDiscordId( UUID discordId);
-//
-//}
+package com.bigschlong.demo.repositories;
+
+import com.bigschlong.demo.models.dtos.DiscordPlayer;
+import com.bigschlong.demo.models.dtos.NbaPlayer;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface DiscordPlayerRepository extends CrudRepository<DiscordPlayer, UUID> {
+
+    @Query(value = """
+    SELECT np.* FROM daily_roster dr
+    JOIN discord_players dp on dp.discord_player_uid = dr.discord_player_uid
+    JOIN  nba_players np on np.nba_player_uid = dr.nba_player_uid
+    WHERE dp.discord_player_uid = :discordId
+    """)
+    List<NbaPlayer> getRosterByDiscordId( UUID discordId);
+
+}
--- src/main/java/com/bigschlong/demo/repositories/ServerRepository.java
+++ src/main/java/com/bigschlong/demo/repositories/ServerRepository.java
@@ -1,23 +1,23 @@
-//package com.bigschlong.demo.repositories;
-//
-//import com.bigschlong.demo.models.dtos.DiscordPlayer;
-//import com.bigschlong.demo.models.dtos.Server;
-//import org.springframework.data.jdbc.repository.query.Query;
-//import org.springframework.data.repository.CrudRepository;
-//import org.springframework.stereotype.Repository;
-//
-//import java.util.List;
-//import java.util.UUID;
-//
-//@Repository
-//public interface ServerRepository extends CrudRepository<Server, UUID> {
-//
-//    @Query(value = """
-//    SELECT dp.* FROM discord_player_servers dps
-//    JOIN servers s on s.server_uid = dps.server_uid
-//    JOIN discord_players dp on dp.player_uid = dps.player_uid
-//    WHERE s.server_uid = :serverId
-//    """)
-//    List<DiscordPlayer> getDiscordPlayersByServerId(UUID serverId);
-//
-//}
+package com.bigschlong.demo.repositories;
+
+import com.bigschlong.demo.models.dtos.DiscordPlayer;
+import com.bigschlong.demo.models.dtos.Server;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface ServerRepository extends CrudRepository<Server, UUID> {
+
+    @Query(value = """
+    SELECT dp.* FROM discord_player_servers dps
+    JOIN servers s on s.server_uid = dps.server_uid
+    JOIN discord_players dp on dp.player_uid = dps.player_uid
+    WHERE s.server_uid = :serverId
+    """)
+    List<DiscordPlayer> getDiscordPlayersByServerId(UUID serverId);
+
+}
