9e9295073521536e3e1fa04d0f71d3589a8f65cb
sriram <srirambhat7@gmail.com>
2026-01-21 17:47:35 -0600

    feat: update CORS configuration to allow multiple origins in ActivitiesController

--- src/main/java/com/picknroll/demo/controllers/ActivitiesController.java
+++ src/main/java/com/picknroll/demo/controllers/ActivitiesController.java
@@ -33,7 +33,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("api/activity")
-@CrossOrigin(origins = "https://1290520169185280062.discordsays.com")
+@CrossOrigin(origins = {"https://1290520169185280062.discordsays.com", "https://1308185877419130891.discordsays.com"})
 public class ActivitiesController {
 
     public static final int MAX_DOLLARS = 100;
