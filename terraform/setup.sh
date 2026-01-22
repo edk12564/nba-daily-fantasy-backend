ffcd289dd7f59c764c3d60fae7bd9feed1a82eae
sriram <srirambhat7@gmail.com>
2026-01-21 18:14:56 -0600

    refactor: streamline environment variable setup in setup.sh

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -17,28 +17,27 @@ sudo rm -rf /opt/nba-daily-fantasy-backend
 sudo mkdir -p /opt/nba-daily-fantasy-backend
 sudo mv /tmp/demo-0.0.1-SNAPSHOT.jar /opt/nba-daily-fantasy-backend/app.jar
 
-if [ ! -f /etc/sysconfig/myapp.env ]; then
-  sudo touch /etc/sysconfig/myapp.env
-  sudo chmod 600 /etc/sysconfig/myapp.env
+sudo rm /etc/sysconfig/myapp.env || true
+sudo touch /etc/sysconfig/myapp.env
+sudo chmod 600 /etc/sysconfig/myapp.env
 
-  # List of all parameters to fetch
-  PARAMS=(
-      "DATABASE_USER"
-      "DATABASE_URL"
-      "DISCORD_API_PUBLIC_KEY"
-      "DISCORD_CLIENT_SECRET"
-      "VITE_DISCORD_CLIENT_ID"
-      "BOT_ACCESS_KEY"
-      "DB_PASSWORD",
-      "JWT_SECRET"
-  )
+# List of all parameters to fetch
+PARAMS=(
+    "DATABASE_USER"
+    "DATABASE_URL"
+    "DISCORD_API_PUBLIC_KEY"
+    "DISCORD_CLIENT_SECRET"
+    "VITE_DISCORD_CLIENT_ID"
+    "BOT_ACCESS_KEY"
+    "DB_PASSWORD",
+    "JWT_SECRET"
+)
 
-  # Loop through and write each parameter to the environment file
-  for PARAM in "${PARAMS[@]}"; do
-      VALUE=$(aws ssm get-parameter --name "$PARAM" --with-decryption --query "Parameter.Value" --output text --region us-east-1)
-      sudo echo "$PARAM=$VALUE" >> /etc/sysconfig/myapp.env
-  done
-fi
+# Loop through and write each parameter to the environment file
+for PARAM in "${PARAMS[@]}"; do
+    VALUE=$(aws ssm get-parameter --name "$PARAM" --with-decryption --query "Parameter.Value" --output text --region us-east-1)
+    sudo echo "$PARAM=$VALUE" >> /etc/sysconfig/myapp.env
+done
 
 FILE="/etc/letsencrypt/live/picknrolls.click/fullchain.pem"
 # create tls cert and move to project directory and give it permissions
